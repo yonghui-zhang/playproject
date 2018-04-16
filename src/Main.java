4d50343dc3db9970c4c3226d4cdfe186b7b7f0f3
yonghui-zhang <1798916974@qq.com>
2018-04-16 19:42:30 +0800

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("env"));
-        System.out.println(System.getenv("ENV"));
-        System.out.println(System.getProperty("os.name"));
-    }
-}
